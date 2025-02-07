0ca953e920b3e9701bf8a50436c5e6f13bbd46e5
krish4221 <krishnakumar944385@gmail.com>
2025-02-07 10:20:15 +0530

    new update

--- src/app/(pages)/scan/page.tsx
+++ src/app/(pages)/scan/page.tsx
@@ -251,7 +251,7 @@ const QRCodeScannerPage = () => {
                   className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:opacity-50"
                   disabled={saving}
                 >
-                  {saving ? 'Saving...' : 'Save Data'}
+                  {saving ? 'Admiting...' : 'Admit'}
                 </button>
               </>
             ) : error ? (
